077df6ae070ead83885bfec65fa643a0a7222d24
Mohammed Sameer <msameer@foolab.org>
2015-07-14 04:45:05 +0300

    android/log.h is not part of glibc so use printf instead for now

--- mosaic/Log.h
+++ mosaic/Log.h
@@ -16,9 +16,8 @@
 #ifndef LOG_H_
 #define LOG_H
 
-#include <android/log.h>
-#define LOGV(...) __android_log_print(ANDROID_LOG_SILENT, LOG_TAG, __VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGV(...) printf(__VA_ARGS__)
+#define LOGI(...) printf(__VA_ARGS__)
+#define LOGE(...) printf(__VA_ARGS__)
 
 #endif
